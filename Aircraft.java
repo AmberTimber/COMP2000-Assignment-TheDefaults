b1c72cacca786ec49f592411377118ea9586a707
Arif Reogir <165476513+ArifReogir@users.noreply.github.com>
2026-08-18 10:29:48 +1000

    Add files via upload

---
+++ Aircraft.java
@@ -0,0 +1,41 @@
+public class Aircraft {
+    private String aircraftID;
+    private String operator;
+    private String model;
+    private double fuelLevel;
+    private int capacity;
+    private String status;
+
+    public Aircraft(String aircraftID, String operator, String model, double fuelLevel, int capacity, String status) {
+        this.aircraftID = aircraftID;
+        this.operator = operator;
+        this.model = model;
+        this.fuelLevel = fuelLevel;
+        this.capacity = capacity;
+        this.status = "GROUNDED"; // Default status of an Aircraft
+    }
+
+    public String getAircraftID() {
+        return aircraftID;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
