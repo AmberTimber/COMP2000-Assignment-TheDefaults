3e72c90aed15b1276cea9361a110ecceba406707
Tim <timothy.chan6@students.mq.edu.au>
2026-08-18 12:01:08 +1000

    added plane types

---
+++ Cargo.java
@@ -0,0 +1,7 @@
+public class Cargo {
+    private double maxWeight;
+
+    public Cargo (double maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+}
---
+++ Commercial.java
@@ -0,0 +1,7 @@
+public class Commercial {
+    private int NoSeats;
+    
+    public Commercial (int NoSeats) {
+        this.NoSeats = NoSeats;
+    }
+}
