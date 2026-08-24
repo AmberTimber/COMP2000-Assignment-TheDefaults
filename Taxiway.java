129e778fcee836a90bf7a6d74dafb825c8403b9b
Yurim Chang <yu-rim.chang@students.mq.edu.au>
2026-08-25 01:30:09 +1000

    Update Taxiway.java
    update on Taxiway class
    
    (TODO: add more variables and methods to accurately represent Taxiway & update Runway.java to extend abstract class AirportPath)

--- Taxiway.java
+++ Taxiway.java
@@ -1,3 +1,57 @@
 public class Taxiway extends AirportPath {
 
+  private final int capacity;
+  private final double maxSpeedKnots;
+
+  /**
+   * 
+   * @param id:             Taxiway id
+   * @param lengthInMeters: Physical length of the taxiway
+   * @param capacity:       Maximum number of aircraft allowed on the taxiway
+   *                        simultaneously
+   * @param maxSpeedKnots:  Ground speed limit for aircraft transit
+   */
+  public Taxiway(String id, double lengthInMeters, int capacity, double maxSpeedKnots) {
+    super(id, lengthInMeters);
+    this.capacity = capacity;
+    this.maxSpeedKnots = maxSpeedKnots;
+  }
+
+  /**
+   * Overridden from AirportPath abstract class.
+   * "Come up with logic"
+   */
+  @Override
+  public boolean canAcceptAircraft(Aircraft aircraft) {
+    return false; // needs to be modified (come up with logic)
+  }
+
+  /**
+   * 
+   * Adds an aircraft to the taxiway is space allows
+   */
+  public boolean enterTaxiway(Aircraft aircraft) {
+    if (canAcceptAircraft(aircraft)) {
+      // logic
+      return true;
+    }
+    return false;
+  }
+
+  public Aircraft exitTaxiWay() {
+    // return
+  }
+
+  // change return val
+  public int getCurrentOccupancy() {
+    return 0;
+  }
+
+  public int getCapacity() {
+    return capacity;
+  }
+
+  public double getMaxSpeedKnots() {
+    return maxSpeedKnots;
+  }
 }
