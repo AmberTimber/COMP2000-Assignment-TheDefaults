afd6e2b198820d2ad1f30ede80e125c8d157b072
uyennguyen-06 <uyen.nguyen3@students.mq.edu.au>
2026-08-29 18:13:58 +1000

    Remove duplicated Airport class

--- Airport.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Airport {
-    private String AirportName;
-    private ArrayList<Runway> runways;
-    private ArrayList<Gate> gates;
-    private ArrayList<Taxiway> taxiways;
-    private ArrayList<Aircraft> Aircraft;
-}
